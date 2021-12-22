ac43ee1f0ef8af90d68438545d30a083666edffd
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2021-12-22 14:03:02 -0300

    refactor(CartResume): updating prop name

--- src/components/CartResume/index.stories.tsx
+++ src/components/CartResume/index.stories.tsx
@@ -11,13 +11,13 @@ const Template: ComponentStory<typeof CartResume> = (args) => <CartResume {...ar
 
 export const NoDiscount = Template.bind({});
 NoDiscount.args = {
-  subTotal: 'R$ 29,80',
+  subtotal: 'R$ 29,80',
   total: 'R$ 29,80'
 };
 
 export const Discount = Template.bind({});
 Discount.args = {
   discount: 'R$ 2,98',
-  subTotal: 'R$ 29,80',
+  subtotal: 'R$ 29,80',
   total: 'R$ 26,82'
 };
